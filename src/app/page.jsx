509ad010e47c4905e336191ebbe5b0987ef76bb6
keysan <ksandaru99@gmail.com>
2024-05-26 15:43:48 +0530

    create app level pages and styles

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import styles from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.description}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src='/brands.png' alt='brands' fill className={styles.brandsImage}/>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src='/hero.gif' alt='hero image' fill className={styles.heroImage}/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
