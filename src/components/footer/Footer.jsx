8f0df5e462b4ed0e935b2535bc0cefcc4a2c7d63
keysan <ksandaru99@gmail.com>
2024-05-26 15:44:50 +0530

    add footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+import styles from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Kanishka Sandaruwan</div>
+      <div className={styles.text}>
+        2024 © All rights reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
